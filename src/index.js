559bb238fca7f9aa408faa73285899867964c43f
Ziskacodes <fr.hennings85@gmail.com>
2025-05-24 10:48:27 +0200

    added the search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 
+function refreshData (response) {
+
+    let temperatureElement = document.querySelector("#temperature");
+    let clearTemperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city-id");
+    cityElement.innerHTML = response.data.city;
+     temperatureElement.innerHTML = Math.round(clearTemperature);
+
+}
+
+function searchCity(city) {
+
+    let apiKey = "o264bt8e7db718ffba5d20417c0b8fa3";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshData);
+}
+
+
 function handleSearchInput (event){
     event.preventDefault();
     {
         let searchInput=document.querySelector("#form-input-id");
-        let cityElement=document.querySelector("#city-id");
-        cityElement.innerHTML = searchInput.value;
+        
+        searchCity(searchInput.value)
     }
 }
 let searchFormElement= document.querySelector(".formular");
 searchFormElement.addEventListener("submit", handleSearchInput);
+
+searchCity("Hamburg");
